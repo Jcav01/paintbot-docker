30e3b945dc8a4fe135e6f8e20a9f213063526528
Jcav <Jcav>
2024-09-30 02:13:40 -0400

    Remove ping command and update edit with placeholder

--- discord/src/commands/paintbot/edit.js
+++ discord/src/commands/paintbot/edit.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('edit')
-		.setDescription('Provides information about the user.'),
+		.setDescription('Will be used to update notifications. Not implemented yet.'),
 	async execute(interaction) {
 		// interaction.user is the object representing the User who ran the command
 		// interaction.member is the GuildMember object, which represents the user in the specific guild
